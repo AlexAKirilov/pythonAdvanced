3345930252355fa0a624ef3645d9b7ebfa625144
AlexAKirilov <146766425+AlexAKirilov@users.noreply.github.com>
2024-03-25 15:38:15 +0500

    added max_number

---
+++ module_2/5.Максимальное число/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/max_number/<path:numbers>')
+def max_number(numbers):
+    list_of_numbers = numbers.split('/')
+    try:
+        list_of_numbers = [float(x) for x in list_of_numbers]
+        max_num = max(list_of_numbers)
+        try:
+            if max_num % 1 == 0:
+                max_num = int(max_num)
+            else:
+                pass
+        except:
+            max_num = max_num
+        return str(max_num)
+    except:
+        return 'Error. Invalid argument type on input'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
